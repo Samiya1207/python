2e33594d89d61250b3ea8b7faddbdb2d73239552
Samiya1207 <120257572+Samiya1207@users.noreply.github.com>
2022-12-14 14:09:00 +0530

    Add files via upload

---
+++ twinkle1.py
@@ -0,0 +1,1 @@
+print("Twinkle, twinkle, Little star,\n\tHow I wonder what you are!\n\t\t Up above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, Little star,\n\tHow I wonder what you are" )
